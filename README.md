afa9273b13862fac084ec5aa8733a730cedf26a1
dan-da <dan-da@users.noreply.github.com>
2016-06-09 16:48:19 -0700

    first commit!

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
 # solarvisor
-A script that monitors battery/charge levels and starts/stops child processes when power is available.
+
+A script that runs loads opportunistically by monitoring battery/charge levels
+to start/stop child processes when power is available.
+
+# description
+
+This script was written with a single goal in mind: to opportunistically run
+heavy energy loads when sunlight is available and stop them when it is not.
+
+Some common types of opportunistic loads are:
+  + bitcoin (or altcoin) mining.
+  + hot water heating
+  + space heating
+  + air conditioning / climate control.
+  + ventilation
+  
+But really how you use it is up to you.  solarvisor doesn't care.
+
+# features
+
+* starts and stops processes (loads) based on current battery voltage.
+* user-definable start/stop voltage levels
+* defaults for common nominal voltages:  12,24,36,48,72
+* supports arbitrary voltages.  nominal voltage not required.
+* failsafe mode daylight time window in case battery voltage cannnot be read.
+* max_stops_per_day setting prevents intermittent running on cloudy days.
+
+# how it operates
+
+solarvisor is invoked with an argument --load-cmd that represents the power
+load. The load-cmd (program) is created by the user and can do anything but
+it is important that it starts the load when executed and stops it when killed.
+
+Let's use a 48v nominal system for example. A minimum voltage and a start
+voltage are defined, by default 51 and 53 respectively.
+
+The script runs in a loop and checks battery voltage each minute.
+
+If the battery voltage is above 53 then the load-cmd is started. If the
+voltage drops below 51 then the load-cmd is killed.
+
+This process continues forever.
+
+The difference between 51 and 53 is necessary to prevent flutter, and is
+adjustable. When the load starts, the voltage will drop somewhat. If it dropped
+below 51 then the load would be stopped, at which point the voltage would rise
+again, and the load would be started again... and so on. The best values to use
+depend on many factors (system size, load size, sunlight, battery size/charge,
+etc). solarvisor has default values for common nominal voltages, but you are
+encouraged to tweak as necessary for your application.
+
+## failsafe mode
+
+At times, the battery voltage may not be available.  In this case, failsafe mode
+activates. In failsafe mode, solarvisor uses a time window during which energy
+is expected to be available. In a solar system, these would be daylight hours.
+
+In this mode the load-cmd will be started when entering the window and
+stopped when leaving the window.
+
+# Flutter prevention and cloudy days.
+
+On cloudy days, there may not be enough energy available to even charge your
+battery much less operate a heavy load.
+
+To help detect and deal with this, solarvisor has a setting max_stops_per_day which
+is set to 1 by default.
+
+So let's say that on a cloudy day the sun pops out for a bit and the voltage
+gets up high enough to start the load the first time. Then a big black cloud
+comes and maybe some rain. The voltage quickly drops down below the min_volts
+setting and the load is stopped. Then the sun comes out again and
+voltage passes the start_volts setting. This time, the load will *not* be
+started because we have already reached the max_stops_per_day setting of 1.
+
+Depending on your situation, you might wish to raise this setting.
+
+# example -- log output
+
+# Let's see an example session over a couple days.
+
+```
+2016-06-09T10:05:22-07:00 -- Battery voltage is 54.5.  (above 53).  starting service
+pid = 18713
+2016-06-09T10:06:24-07:00 -- Battery voltage is 54.4 and service is running.  No action taken
+2016-06-09T10:07:25-07:00 -- Battery voltage is 54.3 and service is running.  No action taken
+...
+2016-06-09T11:08:50-07:00 -- Battery voltage not read. failsafe mode.
+2016-06-09T11:08:50-07:00   -- No action taken.
+
+```
+
+# obtaining battery voltage
+
+The trickiest part of all this is reading the battery's voltage.  Methods for doing
+this can vary from system to system.
+
+Right now, solarvisor makes an http request to a local server running an
+instance of theblackboxproject, which is a PHP web interface to a midnite
+classic controller.
+
+If you are using solarvisor you will need to modify the function
+get_battery_info() to talk to your charge controller or battery monitor system.
+
+In the next release, solarvisor will call an external script that returns
+battery info, usually from a charge controller.
+
+The info will returned in a common, documented format.
+
+
+# Usage
+
+```
+   solarvisor.php
+
+   This script starts and stops processes according to battery level.
+
+   Required:
+    --start-cmd=<cmd>   cmd to start process.
+    --force-start       exec cmd initially irregardless.
+
+   Options:
+
+    -h                Print this help.
+```
+
+
+# Installation and Running.
+
+PHP 5.4+ is required. So far solarvisor has only been tested on Linux. It should
+work on other unix systems including Mac. It will not work on Windows at
+present.
+
+There are no other dependencies.
+
+Assuming you have PHP installed, you can run solarvisor via:
+
+```php solarvisor.php```
+
+or
+
+```./solarvisor.php```
+
+
+
+# Todo Ideas
+
+* Obtain battery info from external script. ( user's can provide their own )
+* Implement events/modes/settings for charge control modes (bulk/absorb/float/sleep)
+* Make a way to signal solarvisor to start/stop load on demand without killing it.
+* cleaner output/display, perhaps ncurses based.
+
+
+# Use at your own risk.
+
+The author makes no claims or guarantees of correctness.
+
---
+++ solarvisor.php
@@ -0,0 +1,424 @@
+#!/usr/bin/env php
+
+<?php
+
+exit( main( $argv ) );
+
+/**
+ * program main().
+ */
+function main( $argv ) {
+    
+    // -- User Adjustable Settings --
+    $settings = [
+        // start of usable daylight power. failsafe in case voltage unavailable.
+       'failsafe-window-starttime' => '9:00',
+       
+        // start of usable daylight power. failsafe in case voltage unavailable.
+       'failsafe-window-stoptime' => '19:00',
+       
+        // prevent flutter, especially on cloudy days.
+       'max-stops-per-day' => 1, 
+    ];
+    // -- End User Adjustable Settings --
+    
+    $params = get_params();
+    $rc = check_params( $params );
+    if( $rc != 0 ) {
+        return $rc;
+    }
+    
+    $params = array_merge( $settings, $params );
+    print_params( $params );
+    
+    setup_signal_handlers();
+    run_main_loop( params_2_settings( $params ) );   
+}
+
+/**
+ * prints settings at startup
+ */
+function print_params( $params ) {
+    echo date('c') . " -- solarvisor starting with these settings:\n---\n";
+    echo json_encode( $params, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    echo "\n---\n\n";
+}
+
+/**
+ * convert params with hyphens to underscores.
+ */
+function params_2_settings( $params ) {
+    $settings = [];
+    foreach( $params as $k => $v ) {
+        $k = str_replace( '-', '_', $k);
+        $settings[$k] = $v;
+    }
+    return $settings;    
+}
+
+/**
+ * main loop of program.  the guts.
+ */
+function run_main_loop( $settings ) {
+    extract( $settings );
+
+    $stops_today = 0;
+    $today = date('Y-m-d');
+
+    $proc = &proc::$proc;
+    $cnt = 0;
+    while( true ) {
+        $running = $proc ? $proc->status() : false;
+        $batt_info = get_battery_info();
+        $volts = @$batt_info['Output Voltage'];  // fixme:  normalize.
+        $time = date('c');
+
+        // reset daily stop counter on date change.
+        if( date('Y-m-d') != $today ) {
+            $stops_today = 0;
+            $today = date('Y-m-d');
+        }
+
+        if( $cnt++ == 0 && $force_start ) {
+           echo sprintf( "$time -- Forcing start because --force-start used.  starting\n" );
+           $proc = new process($load_cmd, $log_file);
+        }
+        else if( !$running && $stops_today >= $max_stops_per_day ) {
+            echo sprintf( "Daily stop limit (%s) reached. no more starts until tomorrow\n" );
+            continue;
+        }
+        // enter failsafe mode if voltage not read.
+        else if( !$volts ) {
+            $startafter = strtotime( $failsafe_window_starttime );
+            $stopafter = strtotime( $failsafe_window_stoptime );
+            echo sprintf( "$time -- Battery voltage not read. failsafe mode.\n", $volts );
+            
+            $ctime = time();
+            if( !$running && $ctime > $startafter && $ctime < $stopafter ) {
+                echo sprintf( "$time   -- within failsafe operation window and service not running. starting\n" );
+                $proc = new process( $load_cmd, $log_file );
+            }
+            else if( $running && $ctime < $startafter && $ctime > $stopafter && $proc ) {
+                echo sprintf( "$time   -- outside failsafe operation window and service is running. stopping\n" );
+                $stops_today ++;
+                $proc->stop();
+            }
+            else {
+                echo sprintf( "$time   -- No action taken.\n" );
+            }
+        }
+        else if( $running && $volts < $volts_min ) {
+            echo sprintf( "$time -- Battery voltage is %s. (below $volts_min).  stopping service\n", $volts ) ;
+            $stops_today ++;
+            $proc->stop();
+        }
+        else if( !$running && $volts >= $volts_start_min ) {
+            echo sprintf( "$time -- Battery voltage is %s.  (above $volts_start_min).  starting service\n", $volts );
+            $proc = new process( $load_cmd, $log_file );
+        }
+        else {
+            echo sprintf( "$time -- Battery voltage is %s and service is %srunning.  No action taken\n", 
+                          $volts,
+                          $running ? '' : 'not ' );
+        }
+        sleep(60);
+    } 
+}
+
+/**
+ * retrieves CLI args
+ */
+function get_params() {
+    
+    $opt = getopt("ha:sp:", [ 'load-cmd:', 'force-start', 'nominal:',
+                              'volts-min:', 'volts-start-min:', 'log-file:'] );
+                             
+    $params['load-cmd'] = @$opt['load-cmd'];
+    $params['force_start'] = isset( $opt['force-start'] );
+    $params['nominal'] = @$opt['nominal'] ?: 48;
+    $params['volts-min'] = @$opt['volts-min'];
+    $params['volts-start-min'] = @$opt['volts-start-min'];
+    $params['log-file'] = @$opt['log-file'] ?: '/dev/null';
+    
+    return $params;
+}
+
+/**
+ * validates CLI args, modifies as needed.
+ * prints error message on any validation error.
+ * returns 0 on success.  else program should exit.
+ */
+function check_params( &$params ) {
+    
+    if( isset($opt['h']) || !($params['load-cmd'] ) ){
+        print_help();
+        return -1;
+    }
+    
+    $nominal = $params['nominal'];
+    $nominal_limits = get_nominal_voltages( $params['nominal'] );
+    
+    if( !@$nominal_limits ) {
+        if( $nominal != 'none' ) {
+            echo sprintf( "warning: nominal voltage '%s' is unknown.  assuming --nominal=none\n", $nominal );
+        }
+        
+        if( !$params['volts-min'] || !$params['volts-start-min'] ) {
+            echo sprintf( "volts-min and volts-start-min are required when nominal=none.\n" );
+            return 1;
+        }
+    }
+    else {
+        // this checks prevent conflicts between --nominal and [--volts-min, --min-start_volts]
+        if( $params['volts-min'] && !between($params['volts-min'], $nominal_limits['range-min'], $nominal_limits['range-max'] )) {
+            echo sprintf( "volts-min %s outside range for nominal voltage: $nominal\n", $params['volts-min'] );
+            return 1;        
+        }
+        else if( $params['volts-start-min'] && !between($params['volts-start-min'], $nominal_limits['range-min'], $nominal_limits['range-max'] )) {
+            echo sprintf( "volts-start-min %s outside range for nominal voltage: $nominal\n", $params['volts-start-min']);
+            return 1;        
+        }
+        // use user-supplied values if supplied, else defaults for specified nominal voltage.
+        $params['volts-min'] = @$params['volts-min'] ?: $nominal_limits['volts-min'];
+        $params['volts-start-min'] = @$params['volts-start-min'] ?: $nominal_limits['volts-start-min'];
+    }
+    
+    
+    if( $params['volts-min'] >= $params['volts-start-min'] ) {
+        echo sprintf( "volts-start-min (%s) must be greater than volts-min (%s)\n", $params['volts-start-min'], $params['volts-min']);
+        return 1;
+    }
+    
+    return 0;
+}
+
+/**
+ * setup signal handlers for CTRL-C, TERM
+ * needed so we can kill load-cmd before shutdown.
+ */
+function setup_signal_handlers() {
+    
+    declare(ticks = 50);
+    pcntl_signal(SIGTERM, 'shutdown_cb');
+    pcntl_signal(SIGINT, 'shutdown_cb');
+    pcntl_signal(SIGCHLD, SIG_IGN);
+}
+
+/**
+ * Returns table of nominal voltages if volts is null.
+ * Otherwise, returns voltage info for specific voltage, or null if not found.
+ */
+function get_nominal_voltages( $volts = null ) {
+    
+    static $nominal_volts;
+    
+    if( $nominal_volts ) {
+        return $nominal_volts;
+    }
+    
+    // I start with 48 nominal because that is why my system uses and it is
+    // easiest for me to think in those numbers.  I then calculate defaults
+    // for other nominal voltages from there.
+    $volts_min_48 = 51;
+    $volts_start_min_48 = 53;
+    $range_min_48 = 40;
+    $range_max_48 = 64;
+    
+    $volts_min_12 = $volts_min_48 / 4;
+    $volts_start_min_12 = $volts_start_min_48 / 4;
+    $range_min_12 = $range_min_48 / 4;
+    $range_max_12 = $range_max_48 / 4;
+    
+    $nominal_volts = [12 => ['volts-min' => $volts_min_12,
+                             'volts-start-min' => $volts_start_min_12,
+                             'range-min' => $range_min_12,
+                             'range-max' => $range_max_12
+                            ],
+                      24 => ['volts-min' => $volts_min_12 * 2,
+                             'volts-start-min' => $volts_start_min_12 * 2,
+                             'range-min' => $range_min_12 * 2,
+                             'range-max' => $range_max_12 * 2
+                            ],
+                      36 => ['volts-min' => $volts_min_12 * 3,
+                             'volts-start-min' => $volts_start_min_12 * 3,
+                             'range-min' => $range_min_12 * 3,
+                             'range-max' => $range_max_12 * 3
+                            ],
+                      48 => ['volts-min' => $volts_min_48,
+                             'volts-start-min' => $volts_start_min_48,
+                             'range-min' => $range_min_48,
+                             'range-max' => $range_max_48
+                            ],
+                      72 => ['volts-min' => $volts_min_12 * 6,
+                             'volts-start-min' => $volts_start_min_12 * 6,
+                             'range-min' => $range_min_12 * 6,
+                             'range-max' => $range_max_12 * 6
+                            ],
+                      ];
+    
+    return $volts ? @$nominal_volts[$volts] : $nominal_volts;
+}
+
+/**
+ * an abstract class to avoid a global var.  todo:  refactor.
+ * ( needed for signal handler callback )
+ */
+abstract class proc {
+    static $proc = null;
+}
+
+/**
+ * retrieves battery info.
+ * @todo: should be abstracted to call various implementations and return
+ *        normalized dataset.
+ */
+function get_battery_info() {
+
+    $url = 'http://192.168.2.201/theblackboxproject/htdocs/real.php';
+    $buf = file_get_contents($url);
+    $lines = explode( "\n", $buf );
+    $info = [];
+    foreach( $lines as $line ) {
+    $row = explode( '|', $line );
+        if( count( $row ) >= 2 ) {
+            $info[$row[0]] = $row[1];
+        }       
+    }
+    return $info;
+}
+
+// handle kill child process when we are killed via SIGTERM or SIGINT
+function shutdown_cb( $signo=null ) {
+    // stop child process if we are terminated.
+    // must call exit, else CTRL-C will not work.
+    echo "\nin signal handler! got signal $signo \n";
+
+    $proc = &proc::$proc;
+    if( $proc && $proc->get_pid() ) {
+        echo sprintf( "stopping child process.  pid=%s...\n", $proc->get_pid() );;
+        $rc = $proc->stop();
+        echo $rc ? "  success!\n" : "  failed. The process is still running, but I must go... \n";
+    }
+
+    echo "exiting!\n";
+    exit(0);
+}
+
+/**
+ * returns true if value is between lower and upper.
+ */
+function between( $v, $lower, $upper ) {
+    return $v >= $lower && $v <= $upper;
+}
+
+
+/**
+ * prints help / usage.
+ */
+function print_help() {
+
+   echo <<< END
+   
+      solarvisor.php
+
+   This script starts and stops processes according to battery level.
+
+   Required:
+    --load-cmd=<cmd>       cmd to start process.
+
+   Options:
+
+    --force-start           exec cmd initially irregardless.
+    --nominal=<volts>       nominal system voltage. 
+                               12,24,36,48,72, or 'none'.  default = 48
+    --volts-min=<v>         minimum voltage before stopping load.
+                               default = 51 unless --nominal is used.
+    --volts-start-min=<v>   minimum voltage before starting load.
+                               default = 53 unless --nominal is used.
+    --log-file=<path>       path to send load-cmd output. default = /dev/null
+    
+    -h                      Print this help.
+
+END;
+
+}
+
+/**
+ * A class to start/stop/status external commands.
+ * @compatibility: Linux only. (Windows does not work).
+ * @author: Peec
+ * heavily modified by danda.
+ */
+class process{
+    private $pid;
+    private $command;
+    private $logpath = '/dev/null';
+
+    public function __construct($cl=false, $logpath){
+        $this->logpath = $logpath;
+        if ($cl != false){
+            $this->command = $cl;
+            $this->run();
+        }
+    }
+    private function run(){
+//        $command = sprintf( 'nohup %s > %s 2>&1 & echo $!', escapeshellcmd($this->command), $this->logpath );
+        $command = sprintf( '%s > %s 2>&1 & echo $!', escapeshellcmd($this->command), $this->logpath );
+        exec($command ,$op);
+        $this->pid = (int)$op[0];
+        echo "pid = " . $this->pid . "\n";
+    }
+
+    public function set_pid($pid){
+        $this->pid = $pid;
+    }
+
+    public function get_pid(){
+        return $this->pid;
+    }
+
+    public function status(){
+        if( !$this->pid ) {
+            return false;
+        }
+        return posix_kill( $this->pid, 0 );
+    }
+
+    public function start(){
+        if ($this->command != '') {
+            $this->run();
+        }
+        return true;
+    }
+
+    public function stop(){
+        if( !$this->pid ) {
+            return false;
+        }
+
+        if( !posix_kill( $this->pid, SIGTERM ) ) {
+           echo posix_strerror( posix_get_last_error() );
+        }
+
+        sleep( 3 );
+        if ($this->status() == false) {
+            $this->pid = null;
+            return true;
+        }
+
+        // kill -9
+        if( !posix_kill( $this->pid, SIGKILL ) ) {
+           echo posix_strerror( posix_get_last_error() );
+        }
+
+        sleep(1);
+
+        if ($this->status() == false) {
+            $this->pid = null;
+            return true;
+        }
+        return false;
+    }
+}
+
+
